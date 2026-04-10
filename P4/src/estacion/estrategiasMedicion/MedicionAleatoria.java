9b69e94792f2def94b2ec9e6aa3b235ec6c03454
mandrilus <aaroncete8@hotmail.com>
2026-04-10 14:16:59 +0200

    paquete sensores

--- P4/src/estacion/estrategiasMedicion/MedicionAleatoria.java
+++ P4/src/estacion/estrategiasMedicion/MedicionAleatoria.java
@@ -1,6 +1,6 @@
 package estacion.estrategiasMedicion;
 
-import estacion.Sensor;
+import estacion.sensores.Sensor;
 
 public class MedicionAleatoria implements EstrategiaMedicion{
     private double probFueraRango;
