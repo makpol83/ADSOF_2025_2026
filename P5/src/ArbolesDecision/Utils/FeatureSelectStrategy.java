95d7846324baf836508438a20b3cd6da934eb8d4
mandrilus <aaroncete8@hotmail.com>
2026-05-08 19:44:10 +0200

    mas doc

--- P5/src/ArbolesDecision/Utils/FeatureSelectStrategy.java
+++ P5/src/ArbolesDecision/Utils/FeatureSelectStrategy.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 import ArbolesDecision.Datasets.LabeledDataSet;
 
+
+/**
+ * Esta interfaz funcional proporciona el método necesario para extraer el nombre de una feature de un LabeledDataset en base a una estrategia.
+ * @param <T> tipo paramétrico del objeto a extraer el nombre de la feature
+ * @param <L> tipo paramétrico de las Labels que proporciona el LabeledDataset
+ */
 @FunctionalInterface
 public interface FeatureSelectStrategy<T extends Comparable<? super T>,L> {
+	
+	/**
+	 * Retorna el nombre de una feature en base a una estrategia
+	 * @param availableFeatures lista de los nombres de las features disponibles
+	 * @param dataset dataset que contiene las features y conoce sus labels
+	 * @return nombre de la feature seleccionada
+	 */
     String selectFeature(List<String> availableFeatures, LabeledDataSet<T,L> dataset);
 }
--- P5/src/ArbolesDecision/Utils/LabelProvider.java
+++ P5/src/ArbolesDecision/Utils/LabelProvider.java
@@ -1,5 +1,17 @@
 package ArbolesDecision.Utils;
 
+
+/**
+ * Interfaz que proporciona el método que deben tener lo LabelProvider. Un LabelProvider es un objeto que etiqueta a otro
+ * @param <T> tipo del objeto al que se etiqueta
+ * @param <L> tipo de la etiqueta
+ */
 public interface LabelProvider<T,L> {
+	
+	/**
+	 * Retorna una label dado un objeto
+	 * @param element objeto a etiquetar
+	 * @return etiqueta del objeto
+	 */
     L getLabel(T element);
 }
